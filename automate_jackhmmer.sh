babead99839a6dcd30daf89aa56e945d106264cd
tzvi909 <94492927+tzvi909@users.noreply.github.com>
2022-03-11 16:46:56 +0000

    Bash script for running hmmer on everything

---
+++ automate_jackhmmer.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+module load HMMER/3.3.2-gompi-2021a;
+####
+n=2
+p=4
+for f in $(ls Genes/*.fasta); do
+echo "Processing file: ${f}";
+jackhmmer -N $n -A ${f}.sto --incE 0.0001 --cpu $p ${f} SourceFiles/VFDB_setB_pro.fas;
+done
+mv Genes/*.sto ../AlnFiles/*
+####
+for f in $(ls AlnFiles/*); do
+echo "building HMM: ${f}";
+hmmbuild Profile_HMM/${f}_profile.hmm ${f};
+done
+cat Profile_HMM/*_profile.hmm > Yersinia_HMM.dat
+hmmpress Yersinia_HMM.dat
